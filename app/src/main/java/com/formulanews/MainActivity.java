dd089fea8c9f96d113197974dc3402d7b405f4e6
sgsfabiano <sgsfabiano@gmail.com>
2019-08-15 22:33:53 -0300

    Included BottomNavigationView listeners

--- app/src/main/java/com/formulanews/MainActivity.java
+++ app/src/main/java/com/formulanews/MainActivity.java
@@ -1,14 +1,40 @@
 package com.formulanews;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity
+                          implements  BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mBottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_news: {
+                break;
+            }
+            case R.id.action_standings: {
+                break;
+            }
+            case R.id.action_videos: {
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    private BottomNavigationView mBottomNavigationView;
 }
