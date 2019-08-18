c5962ffefb54e03c639a41f63d8d7b2c67f64933
sgsfabiano <sgsfabiano@gmail.com>
2019-08-18 15:54:46 -0300

    Added open and close fragments management functions.

--- app/src/main/java/com/formulanews/MainActivity.java
+++ app/src/main/java/com/formulanews/MainActivity.java
@@ -19,10 +19,58 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/*
         this.mBottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
         this.mBottomNavigationView.setOnNavigationItemSelectedListener(this);
+*/
 
+        this.initGui();
+
+        /*
+        Fragment[] newsFragment = new NewsFragment[15];
+
+        for(int i=0; i<15; i++) {
+            NewsFragment nf = new NewsFragment("Headline "+ i, "Texto da notícia " + i);
+            newsFragment[i] = nf;
+        }
+
+        openFragment(newsFragment);
+        */
+
+        this.openNewsPage();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_news: {
+                this.closeAllFragments();
+                this.openNewsPage();
+                break;
+            }
+            case R.id.action_standings: {
+                this.closeAllFragments();
+                break;
+            }
+            case R.id.action_videos: {
+                this.closeAllFragments();
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    public void initGui() {
+        this.mBottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        this.mBottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    public void openMainPage() {
+
+    }
+
+    public void openNewsPage() {
         Fragment[] newsFragment = new NewsFragment[15];
 
         for(int i=0; i<15; i++) {
@@ -33,21 +81,8 @@ public class MainActivity extends AppCompatActivity
         openFragment(newsFragment);
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()) {
-            case R.id.action_news: {
-                break;
-            }
-            case R.id.action_standings: {
-                break;
-            }
-            case R.id.action_videos: {
-                break;
-            }
-        }
+    public void openStandingsPage() {
 
-        return true;
     }
 
     private BottomNavigationView mBottomNavigationView;
@@ -62,5 +97,11 @@ public class MainActivity extends AppCompatActivity
         }
 
         transaction.commit();
+    }
+
+    private void closeAllFragments() {
+        for (Fragment f : getSupportFragmentManager().getFragments()) {
+            getSupportFragmentManager().beginTransaction().remove(f).commit();
+        }
     }
 }
