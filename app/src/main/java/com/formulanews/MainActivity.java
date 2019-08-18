b40845c19a844303359906674066db8ea5081c09
sgsfabiano <sgsfabiano@gmail.com>
2019-08-18 13:42:31 -0300

    Multiple fragments working and scrolling.

--- app/src/main/java/com/formulanews/MainActivity.java
+++ app/src/main/java/com/formulanews/MainActivity.java
@@ -3,6 +3,7 @@ package com.formulanews;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
@@ -10,9 +11,6 @@ import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
                           implements BottomNavigationView.OnNavigationItemSelectedListener {
@@ -25,7 +23,13 @@ public class MainActivity extends AppCompatActivity
         this.mBottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
         this.mBottomNavigationView.setOnNavigationItemSelectedListener(this);
 
-        Fragment newsFragment = NewsFragment.newInstance();
+        Fragment[] newsFragment = new NewsFragment[15];
+
+        for(int i=0; i<15; i++) {
+            NewsFragment nf = new NewsFragment("Notícia "+ i);
+            newsFragment[i] = nf;
+        }
+
         openFragment(newsFragment);
     }
 
@@ -33,9 +37,6 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()) {
             case R.id.action_news: {
-                getSupportActionBar().setTitle("Notícias");
-                Fragment newsFragment = NewsFragment.newInstance();
-                openFragment(newsFragment);
                 break;
             }
             case R.id.action_standings: {
@@ -50,11 +51,16 @@ public class MainActivity extends AppCompatActivity
     }
 
     private BottomNavigationView mBottomNavigationView;
+    private FragmentManager mFragmentManager;
+    private FragmentTransaction mFragmentTransaction;
 
-    private void openFragment(Fragment fragment) {
+    private void openFragment(Fragment[] fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, fragment);
-        transaction.addToBackStack(null);
+
+        for(int i=0; i<15; i++) {
+            transaction.add(R.id.container, fragment[i], "News"+i);
+        }
+
         transaction.commit();
     }
 }
--- app/src/main/java/com/formulanews/NewsFragment.java
+++ app/src/main/java/com/formulanews/NewsFragment.java
@@ -17,12 +17,23 @@ public class NewsFragment extends Fragment {
         headline.setText("Aqui vai o headline da notícia");
 
         TextView fullnews = (TextView)view.findViewById(R.id.text_view_news);
-        fullnews.setText("Nascido em Emmerich am Rhein, Renânia do Norte-Vestfália, Alemanha Ocidental, Hülkenberg fez sua estréia de kart em 1997, aos 10 anos de idade. Em 2002, foi Campeão Alemão de Kart Júnior e no ano seguinte ganhou o Campeonato Alemão de Kart. Hülkenberg foi anteriormente gerenciado por Willi Weber, o gerente de longa data de Michael Schumacher. Weber previu que Hülkenberg estaria pronto para a Fórmula 1 até 2008. Ele também elogiou Hülkenberg como um \"talento inacreditável\" e disse que ele lembrou Schumacher é um jovem piloto. Ele também afirmou que ele o apelidou de \"The Hulk\", depois do super-herói fictício, em referência a Hülkenberg mudando sua personalidade ao volante.");
+        fullnews.setText(this.mFullNews);
 
         return view;
     }
 
-    public static NewsFragment newInstance() {
-        return new NewsFragment();
+    public NewsFragment(String news) {
+        this.mFullNews = news;
     }
+
+    /*public static NewsFragment newInstance() {
+        return new NewsFragment();
+    }*/
+
+    public void setFullNews(String s) {
+
+    }
+
+    private String mFullNews;
+
 }
